2991270bc3b37858e83e888774fe579ddd4b5cff
Gary Abramson <gary.abramson@gmail.com>
2017-10-25 23:40:04 -0500

    fixed pylint issues

--- test_knapsack.py
+++ test_knapsack.py
@@ -2,9 +2,9 @@ import KnapsackPacker
 
 def test_knapsack():
     knapsack_packer = KnapsackPacker.KnapsackPacker(20, 5)
-    knapsack_packer.add_item(2,3)
-    knapsack_packer.add_item(3,4)
-    knapsack_packer.add_item(4,5)
-    knapsack_packer.add_item(5,8)
-    knapsack_packer.add_item(9,10)
-    assert 26 == knapsack_packer.get_max_value()
+    knapsack_packer.add_item(2, 3)
+    knapsack_packer.add_item(3, 4)
+    knapsack_packer.add_item(4, 5)
+    knapsack_packer.add_item(5, 8)
+    knapsack_packer.add_item(9, 10)
+    assert knapsack_packer.get_max_value() == 26
