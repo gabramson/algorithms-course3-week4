574d386818658bd6a10a2bcfe618163710e8b1e0
Gary Abramson <gary.abramson@gmail.com>
2017-10-26 22:58:13 -0500

    naive implementation successful

--- KnapsackPacker.py
+++ KnapsackPacker.py
@@ -3,9 +3,9 @@ import numpy as np
 class KnapsackPacker:
     def __init__(self, max_weight, items_count):
         self.max_weight = max_weight
-        self.max_item=0
+        self.max_item = 0
         self.items_count = items_count
-        self.items = np.empty([items_count,2])
+        self.items = np.empty([items_count, 2])
 
     def add_item(self, weight, value):
         self.items[self.max_item][0] = weight
@@ -15,16 +15,18 @@ class KnapsackPacker:
     def get_max_value(self):
         last_values = np.zeros(self.max_weight+1)
         next_values = np.empty(self.max_weight+1)
-        for i in range(0,self.items_count):
+        for i in range(0, self.items_count):
+            if i%100 == 0:
+                print(i)
             w = int(self.items[i][0])
             v = int(self.items[i][1])
             for x in range(0, self.max_weight+1):
-                if w>x:
+                if w > x:
                     next_values[x] = last_values[x]
-                    continue    
+                    continue
                 next_values[x] = self.get_maximum(last_values[x], last_values[x-w]+v)
             last_values = np.copy(next_values)
-        return (next_values[self.max_weight])
+        return next_values[self.max_weight]
 
     def get_maximum(self, a, b):
         return a if a > b else b
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 """
 Module Docstring
 """
+import KnapsackPacker
+
 
 __author__ = "Your Name"
 __version__ = "0.1.0"
@@ -10,8 +12,13 @@ __license__ = "MIT"
 
 def main():
     """ Main entry point of the app """
-    print("hello world")
-
+    myfile = open("knapsack_big.txt", "r")
+    parameters = myfile.readline().split()
+    knapsack_packer = KnapsackPacker.KnapsackPacker(int(parameters[0]), int(parameters[1]))
+    for line in myfile:
+        item = line.split()
+        knapsack_packer.add_item(int(item[1]), int(item[0]))
+    print(knapsack_packer.get_max_value())
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
